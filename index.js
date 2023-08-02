17ae779652dd95e65698e597f8bccc410c6f125e
TarasYarovyi <yarovoy.taras@gmail.com>
2023-08-02 18:24:13 +0200

    MongoDB connected

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log("SERWER WORKING");
 import express from "express";
+import mongoose from "mongoose";
+
 const PORT = 5501;
+const DB_URL = `mongodb+srv://yarovoytaras:Wypoo67Opcb8@cluster0.zq1aqiu.mongodb.net/?retryWrites=true&w=majority`;
 const app = express();
 app.use(express.json());
 
@@ -9,5 +12,15 @@ app.post("/", (req, res) => {
 
   res.status(200).json("Serwer działa!!!");
 });
-
-app.listen(PORT, () => console.log("SERWER START ON PORT " + PORT));
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => console.log("SERWER START ON PORT " + PORT));
+  } catch (e) {
+    console.log(e);
+  }
+}
+startApp();
