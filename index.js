5876b60d112d4cf685228f47523697f6d583de93
TarasYarovyi <yarovoy.taras@gmail.com>
2023-08-05 11:24:02 +0200

    post updated

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-import Post from "./Post.js";
+import router from "./router.js";
 
 const PORT = 5501;
 const DB_URL = `mongodb+srv://yarovoytaras:Wypo67Opcb8@cluster0.zq1aqiu.mongodb.net/?retryWrites=true&w=majority`;
 const app = express();
 app.use(express.json());
-
-app.post("/", async (req, res) => {
-  try {
-    const { author, title, content, picture } = req.body;
-    const post = await Post.create({ author, title, content, picture });
-    console.log(req.body);
-
-    res.json(post);
-  } catch (e) {
-    res.status(500).json(e);
-  }
-});
+app.use("/api", router);
+// app.use("/users", userRouter);
 
 async function startApp() {
   try {
