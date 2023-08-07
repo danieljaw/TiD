65af26db114355169dadff19994b2cc790b4bb01
TarasYarovyi <yarovoy.taras@gmail.com>
2023-08-07 16:23:52 +0200

    updated getOne, update and delete functions

--- PostController.js
+++ PostController.js
@@ -23,18 +23,38 @@ class PostController {
   }
   async getOne(req, res) {
     try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "ID not specified " });
+      }
+      const post = await Post.findById(id);
+      return res.json(post);
     } catch (e) {
       res.status(500).json(e);
     }
   }
   async update(req, res) {
     try {
+      const post = req.body;
+      if (!post._id) {
+        res.status(400).json({ message: "ID not specified " });
+      }
+      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {
+        new: true,
+      });
+      return res.json(updatedPost);
     } catch (e) {
       res.status(500).json(e);
     }
   }
   async delete(req, res) {
     try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "ID not specified " });
+      }
+      const post = await Post.findByIdAndDelete(id);
+      return res.json(post);
     } catch (e) {
       res.status(500).json(e);
     }
